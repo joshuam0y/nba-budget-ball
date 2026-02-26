ac4ec307a35044ca413180f97aa3ee9b9db86898
Joshua Moy <jmoy2077@gmail.com>
2026-02-26 01:29:35 -0500

    3 new songs

--- src/App.jsx
+++ src/App.jsx
@@ -547,7 +547,7 @@ const audioRef = useRef(null);
 const trackIndex = useRef(0);
 const hasStarted = useRef(false);
 
-const TRACKS = ['onepunch.mp3', 'photograph.mp3', 'ateam.mp3', 'cold.mp3','lemonade.mp3', 'outstanding.mp3', 'amazing.mp3', 'bestfriend.mp3', 'baddecisions.mp3'];
+const TRACKS = ['weonit.mp3','lovenwantiti.mp3','poppin.mp3','onepunch.mp3', 'photograph.mp3', 'ateam.mp3', 'cold.mp3','lemonade.mp3', 'outstanding.mp3', 'amazing.mp3', 'bestfriend.mp3', 'baddecisions.mp3'];
 
 const playTrack = (index) => {
   const audio = audioRef.current;
